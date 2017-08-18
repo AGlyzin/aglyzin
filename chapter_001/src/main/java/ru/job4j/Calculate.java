f91e7354c7729e0b716c5b01c645b2ccd4190dea
AGlyzin <aglizin@yandex.ru>
2017-08-18 21:02:13 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,20 @@
 package ru.job4j;
 
+/**
+ * Class Класс для каких-либо вычислений 
+ * @author aglizin
+ * @since 18.08.2017
+ * @version 1
+ */
+
 public class Calculate {
+
+ /**
+ * Main Главный метод - с него всё начинается
+ *@return выводим на экран "Hello world".
+ */
     public static void main(String[] args) {
  	System.out.println("Hello world");
    }
 }
+
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Arthur Glyzin (mailto:aglizin@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
