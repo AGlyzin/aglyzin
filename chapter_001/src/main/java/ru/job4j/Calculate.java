cab3ae5fa904df7d0d7b9937466262bd10019215
AGlyzin <aglizin@yandex.ru>
2017-08-21 13:44:45 +0300

    Checkstyle 4123

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,6 @@
 package ru.job4j;
 
-/**
- * Class Класс для каких-либо вычислений 
+/**Class Класс для каких-либо вычислений.
  * @since 28/08/2017
  * @version 1
  */
@@ -9,8 +8,9 @@ package ru.job4j;
 public class Calculate {
 
  /**
- * Main Главный метод - с него всё начинается
- *@return выводим на экран "Hello world".
+ * Main Главный метод - с него всё начинается.
+ * Выводим в консоль - "Hello world".
+ *@param args - args.
  */
     public static void main(String[] args) {
  	System.out.println("Hello World");
--- chapter_001/src/main/java/ru/job4j/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
-* Package for calculate task.
-*
-* @author Arthur Glyzin (mailto:aglizin@yandex.ru)
-* @version $Id$
-* @since 0.1
-*/
-package ru.job4j;
