ea13a30288ab6b03787a16f2bdad8f4819ee3adb
AGlyzin <aglizin@yandex.ru>
2017-09-03 19:20:57 +0300

    Max of three numbers [#190]

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -1,8 +1,18 @@
 package ru.job4j.max;
 
 public class  Max {
-    public  int max(int first, int second) {
-        return (first > second) ? first : second;
+    public int max(int first, int second) {
+
+      return (first > second) ? first : second;
 
     }
+
+    public int max2(int first, int second, int third) {
+
+        return max(max(first, second), third);
+
+    }
+
 }
+
+
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -9,21 +9,21 @@ public class MaxTest{
     @Test
     public void whenFirstLessSecond() {
         Max maxim = new Max();
-        int result = maxim.max(1, 2);
-        assertThat(result, is(2));
+        int result = maxim.max2(6, 4, 5);
+        assertThat(result, is(6));
     }
 
     @Test
     public void whenThreeMoreSecond() {
         Max maxim = new Max();
-        int result = maxim.max(3, 2);
-        assertThat(result, is(3));
+        int result = maxim.max2(8, 16,22);
+        assertThat(result, is(22));
     }
 
     @Test
-    public void whenOneLessFive() {
+    public void whenOneLessFiveLess6() {
         Max maxim = new Max();
-        int result = maxim.max(1, 5);
-        assertThat(result, is(5));
+        int result = maxim.max2(9, 5,6);
+        assertThat(result, is(9));
     }
 }
