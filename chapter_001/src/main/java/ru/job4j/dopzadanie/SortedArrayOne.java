24689c6a03d2025f3ba2e32c0d2893cc36644db5
AGlyzin <aglizin@yandex.ru>
2017-10-08 19:04:34 +0300

    OneTaskSortedArray [#27222]

---
+++ chapter_001/src/main/java/ru/job4j/dopzadanie/SortedArrayOne.java
@@ -0,0 +1,33 @@
+package ru.job4j.dopzadanie;
+
+/**
+ * class SortedArrayOne - checks sorted
+ *
+ * @author AGlyzin
+ * @version 1
+ * @since 08.10.2017
+ */
+
+public class SortedArrayOne {
+
+    /**
+     * @param value  - input array
+     * @param revers - input constant
+     * @return sorted or not/ true or false
+     */
+
+    public boolean isSorted(int[] value, boolean revers) {
+        if (revers == false) {
+            for (int i = 1; i < value.length; i++) {
+                if (value[i] < value[i - 1]) return false;
+
+            }
+        } else {
+            for (int i = 1; i < value.length; i++) {
+                if (value[i] > value[i - 1]) return false;
+
+            }
+        }
+        return true;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/dopzadanie/SortedArrayOneTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.dopzadanie;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Class SortedArrayOneTest - testing class SortedArrayOne
+ *
+ * @author AGlyzin
+ * @version 1
+ * @since 08.10.2017
+ */
+
+public class SortedArrayOneTest {
+
+    /**
+     * whenCheckTheSortingOfAnArrayPoVozvrastaniy - тест-метод для проверки отсортирован ли массив по возврастанию
+     */
+
+    @Test
+    public void whenCheckTheSortingOfAnArrayPoVozvrastaniy() {
+        SortedArrayOne sortedArrayOne = new SortedArrayOne();
+        int[] value = {4, 7, 9, 10, 22};
+        boolean revers = false;
+        boolean expected = true;
+        boolean result = sortedArrayOne.isSorted(value, revers);
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * whenCheckTheSortingOfAnArrayPoUbyvaniy - тест-метод для проверки отсортирован ли массив по убыванию
+     */
+
+    @Test
+    public void whenCheckTheSortingOfAnArrayPoUbyvaniy() {
+        SortedArrayOne sortedArrayOne = new SortedArrayOne();
+        int[] value = {9, 7, 4, 2, 1};
+        boolean revers = true;
+        boolean expected = true;
+        boolean result = sortedArrayOne.isSorted(value, revers);
+        assertThat(result, is(expected));
+    }
+}
